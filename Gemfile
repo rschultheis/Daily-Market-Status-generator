c8e8687b3783df825a8ccd9e7f69bd7e00050664
rschultheis <robert.schultheis@gmail.com>
2012-02-20 18:10:38 -0500

    got a working page publish.  moved google site configuration into a .yml file which is not checked in

--- Gemfile
+++ Gemfile
@@ -1,14 +1,19 @@
 source :rubygems
 
 #for reading csv files into useful data structures
-gem "csv-mapper"
+gem "csv-mapper", "0.5.1"
 
 #for interacting with yahoo finance api
-gem "json"
+gem "json", "1.6.1"
 
 #for creating data structures of daily quotes
-gem "ohash"
+gem "ohash", "0.0.3"
 
 #for generating charts
-gem "gruff"
-gem "rmagick"
+gem "gruff", "0.3.6"
+gem "rmagick", "2.13.1"
+
+#for publishing to google sites
+gem "gdata", "1.1.2"
+#gem "xml-simple", "1.1.1"
+#gem "libxml-ruby", "2.2.2"
--- lib/dd_publish.rb
+++ lib/dd_publish.rb
@@ -1,44 +1,87 @@
 require 'dd_logger'
 require 'gdata'
+require 'xmlsimple'
 
-class GooglePublisher
+class GoogleSitesPublisher
   
   def initialize(username, password, site)
     @username, @password, @site = username, password, site
     
-    @gapi = GData::Client::Apps.new(:version => 1.4)
+    @base_url = 'https://sites.google.com'
+    @domain = 'site'
+    
+    @gapi = GData::Client::Apps.new(:version => 1.4)  #google sites api requires version 1.4 for some reason
     @gapi.source = 'indexdailydeets-0.1'
-    @gapi.clientlogin(@username, @password, nil, nil, 'jotspot')         
+    @gapi.clientlogin(@username, @password, nil, nil, 'jotspot')  #http://code.google.com/intl/en/apis/sites/faq.html#AuthServiceName
+             
   end
   
-  def feeds_content_site
-    @gapi.get("https://sites.google.com/feeds/content/site/#{@site}").body
+  def site_content
+    body = @gapi.get("#{@base_url}/feeds/content/#{@domain}/#{@site}").body
+    xml = XmlSimple.xml_in(body)
+    
+    pretty_xml = XmlSimple.xml_out(xml)
+    
+    puts
+    puts pretty_xml
+    puts
+    
   end
   
+  def add_page(title, content)       
+    payload = %Q|
+    <entry xmlns="http://www.w3.org/2005/Atom">
+        <category scheme="http://schemas.google.com/g/2005#kind"
+            term="http://schemas.google.com/sites/2008#webpage" label="webpage"/>
+        <title>#{title}</title>
+        <content type="xhtml">
+          <div xmlns="http://www.w3.org/1999/xhtml">#{content}</div>
+        </content>
+      </entry>
+    |
+    
+    puts "post new page:\n" + payload + "\n"
+    
+    @gapi.post("#{@base_url}/feeds/content/#{@domain}/#{@site}", payload)
+  end
+  
+  def attach_file(filepath, parent_page)
+    
+  end  
   
 end
 
-
-module DD_Publish
+require 'yaml'
+module DD_Publisher
   
+  config = YAML.load_file('google_sites.yml')
   
+  PUBLISHER = GoogleSitesPublisher.new(config['username'], config['password'], config['site'])
   
+  def publish(directory='output')    
+    html = File.read(File.join(directory,'goog_site.html'))    
+    PUBLISHER.add_page('Fri Feb 17 2012 SnP 500', html)    
+  end
   
 end
-
 
 if __FILE__ == $0
    require 'test/unit'
 
-  #include DD_Publish
+  include DD_Publisher
 
   TEST_HTML_TO_PUBLISH = 'output/goog_site.html'
   
   class TestDDPublish < Test::Unit::TestCase
     
-    def test_google_publish
+    def xtest_google_publish
       gp = GooglePublisher.new('username','password', 'site_name')
-      puts gp.feeds_content_site
+      
+      gp.site_content          
+    end
+    
+    def test_publish_output_directory
+      publish('output')
     end
   end
     
